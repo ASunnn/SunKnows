e9d688aed1ed1032e46f4a1fe684528295489d9b
ASun <liurf923@gmail.com>
2018-10-05 21:24:16 +0800

    10.5 update

--- src/sunnn/knows/test/Test.java
+++ src/sunnn/knows/test/Test.java
@@ -1,5 +1,12 @@
 package sunnn.knows.test;
 
+import com.sun.corba.se.spi.orbutil.threadpool.ThreadPool;
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * 数据结构
  * 计算机网络
@@ -10,7 +17,13 @@ package sunnn.knows.test;
  *          Set
  *          Map
  *          线程安全
- *      多线程
+ *      并发
+ *          多线程
+ *          锁
+ *          CAS
+ *          volatile
+ *          阻塞队列
+ *          *底层原理
  *      虚拟机
  *      网络编程
  *      其他
@@ -26,6 +39,25 @@ package sunnn.knows.test;
  */
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Thread t = new Thread(new R());
+        t.start();
+        t.join();
+
+        System.out.println("Work Complete");
+    }
+
+    static class R implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Work Start");
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
+
